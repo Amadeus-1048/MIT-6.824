2eb1e021edd6acf5beed1e28259582562817740e
amadeus <amadeus1048@outlook.com>
2023-11-01 14:15:57 +0800

    feat: mapreduce worker

--- 6.5840/src/main/mrsequential.go
+++ 6.5840/src/main/mrsequential.go
@@ -7,12 +7,14 @@ package main
 // go run mrsequential.go wc.so pg*.txt
 //
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 import "6.5840/mr"
 import "plugin"
 import "os"
 import "log"
-import "io/ioutil"
 import "sort"
 
 // for sorting by key.
@@ -42,7 +44,7 @@ func main() {
 		if err != nil {
 			log.Fatalf("cannot open %v", filename)
 		}
-		content, err := ioutil.ReadAll(file)
+		content, err := io.ReadAll(file)
 		if err != nil {
 			log.Fatalf("cannot read %v", filename)
 		}
