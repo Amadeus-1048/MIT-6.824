30f5d317305b603fcfefa439d8e2f87ec97ee783
amadeus <amadeus1048@outlook.com>
2024-01-18 17:42:50 +0800

    feat: refactor persist

--- 6.5840/src/raft/raft_persist.go
+++ 6.5840/src/raft/raft_persist.go
@@ -26,6 +26,11 @@ func (rf *Raft) persist() {
 
 }
 
+// 需要持久化的状态：currentTerm, votedFor, log[]
+// 持久化的时机是需要持久化的状态发生改变的时候
+// log[]是唯一能用来重建应用程序状态的信息，所以Log必须要被持久化存储。
+// votedFor是为了避免在一个term内，节点对其它两个节点投票，造成脑裂的情况。
+// currentTerm被持久化，是为了防止网络分区的情况下（Leader写入了某个term的日志，但是该日志还没有追加给其它节点），一个分区内的节点宕机再恢复，如果只看日志决定term，会导致选择的term过旧，一个term内写的日志在两个分区内不一致。
 func (rf *Raft) encodeState() []byte {
 	w := new(bytes.Buffer)
 	e := labgob.NewEncoder(w)
