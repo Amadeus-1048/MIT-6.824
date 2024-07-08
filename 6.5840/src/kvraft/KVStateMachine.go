9c32f7bd6c17e26e7fe00c257c660cfd37be0f9b
Amadeus <Amadeus1048@outlook.com>
2024-07-08 22:45:16 +0800

    update KVStateMachine service

---
+++ 6.5840/src/kvraft/KVStateMachine.go
@@ -0,0 +1,36 @@
+package kvraft
+
+// 键值状态机接口
+type kVStateMachine interface {
+	Get(key string) (string, Err)
+	Put(key, value string) Err
+	Append(key, value string) Err
+}
+
+// 基于内存的键值存储实现
+type MemoryKV struct {
+	KV map[string]string	// 存储键值对
+}
+
+func NewMemoryKV() *MemoryKV {
+	return &MemoryKV{
+		KV: make(map[string]string)
+	}
+}
+
+func (memoryKV *MemoryKV) Get(key string) (string, Err) {
+	if value, ok := memoryKV.KV[key]; ok {
+		return value, OK
+	}
+	return "", ErrNoKey
+}
+
+func (memoryKV *MemoryKV) Put(key, value string) Err {
+	memoryKV[key] = value
+	return OK
+}
+
+func (memoryKV *MemoryKV) Append(key, value string) Err {
+	memoryKV[key] += value
+	return OK
+}
